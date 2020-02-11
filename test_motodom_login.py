db01099352f17e7afb3b78268566e6ae6e7d1660
Shark <boykoanatoliyo@gmail.com>
2020-02-11 12:09:01 +0200

    test_login + tuning

---
+++ test_motodom_login.py
@@ -0,0 +1,6 @@
+# Created by Shark at 11.02.2020
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+def test_motodom_login():
+    driver = WebDriver(executable_path="C://chromedriver.exe")
+    print(None)
