a441ca3a7d8e342242e5a8e543ff4f6569c0a13f
Boyko Anatoliy <boykoanatoliyo@gmail.com>
2022-12-11 12:44:06 +0200

    update tests

--- test_add_to_cart.py
+++ test_add_to_cart.py
@@ -1,18 +1,26 @@
 # Created by Shark at 11.02.2020
-from selenium.webdriver.chrome.webdriver import WebDriver
 import time
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+driver_service = Service(executable_path='C://chromedriver.exe')
+driver = webdriver.Chrome(service=driver_service)
 
 def test_add_to_cart():
-    driver = WebDriver(executable_path='C://chromedriver.exe')
-    driver.get('https://motodom.com.ua')
-    driver.find_elements_by_xpath("//div[@class='name']//a[contains(text(), 'Мотоботы')]")[2].click()
-    find_name = driver.find_element_by_xpath("//h1[@class]").text
-    choice_size = driver.find_elements_by_xpath(".//label[contains(text(), 'Размер')]//..//ul//li")
-    choice_size[0].click()
-    choice_color = driver.find_elements_by_xpath(".//label[contains(text(), 'Цвет')]//..//ul//li")
-    choice_color[0].click()
-    driver.find_element_by_xpath(".//button[@id='button-cart']").click()
-    time.sleep(15)
-    driver.find_element_by_xpath("//div[@id='cart']//button[@data-toggle='dropdown']").click()
-    driver.find_element_by_xpath("//a[contains(text(), 'Открыть корзину')]").click()
-    assert find_name == driver.find_element_by_xpath("//form//div//table//tbody//td[@class='text-left name']//a").text
+    driver.get('https://motodom.ua')
+    find_goods = driver.find_elements(By.XPATH, ".//div[@class='name']//a[contains(text(), 'Oxford')]") #[10].click()
+    find_goods[0].click()
+    find_name = driver.find_element(By.XPATH, ".//h1[@class]").text.lower()
+    # choice_size = driver.find_elements(By.XPATH, ".//label[contains(text(), 'Размер')]//..//ul//li")
+    #print(choice_size)
+    #choice_size[0].click()
+    choice_color = driver.find_element(By.XPATH, ".//span[@class='option-value']")
+    choice_color.click()
+    driver.find_element(By.XPATH, ".//a[@id='button-cart']").click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, ".//div[@id='cart']").click()
+    driver.find_element(By.XPATH, ".//a[@class='btn-cart btn']").click()
+    assert find_name == driver.find_element(By.XPATH, ".//td[@class='name']//a[contains(text(), 'Подшлемник Oxford Cotton')]").text.lower()
--- test_motodom_login.py
+++ test_motodom_login.py
@@ -1,15 +1,22 @@
 # Created by Shark at 11.02.2020
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
+import allure
+from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+from test_add_to_cart import driver
 
 
 def test_motodom_login():
-    driver = WebDriver(executable_path='C://chromedriver.exe')
-    driver.get('https://motodom.com.ua/index.php?route=account/login')
+    #driver_service = Service(executable_path='C://chromedriver.exe')
+    #driver = webdriver.Chrome(service=driver_service)
+    driver.get('https://motodom.ua/index.php?route=account/login')
     driver.implicitly_wait(3)
-    search_login = driver.find_element_by_xpath('//input[@id="input-email"]')
-    search_password = driver.find_element_by_xpath('//input[@id="input-password"]')
-    search_button_login = driver.find_element_by_xpath("//input[@type='submit']")
+    search_login = driver.find_element(By.XPATH, '//input[@id="input-email"]')
+    search_password = driver.find_element(By.XPATH, '//input[@id="input-password"]')
+    search_button_login = driver.find_element(By.XPATH, "//button[@type='submit']")
     search_login.send_keys("volosorgua@gmail.com")
     search_password.send_keys("123456789")
     search_button_login.click()
-    assert driver.find_element_by_xpath("//div[@class='content my-account']")
+    assert driver.find_element(By.XPATH, "//div[@class='my-account']")
